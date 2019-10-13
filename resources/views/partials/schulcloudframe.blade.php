36541584ca51a6048b73195747eeb8b7c3bfa1f8
Janek Thomaschewski <janek@thomaschewski.dev>
2019-10-13 23:48:27 +0200

    Hide Schul-Cloud User-iFrame if page is embedded
    Implements https://ticketsystem.schul-cloud.org/browse/SC-2141

--- resources/views/partials/schulcloudframe.blade.php
+++ resources/views/partials/schulcloudframe.blade.php
@@ -1,4 +1,10 @@
-{{-- Show iframe from schulcloud if any --}}
+{{--
+This templates embeds an iFrame provided by the Schul-Cloud OAuth Integration.
+The iFrame provides information about the associated Schul-Cloud user without breaking pseudonymization.
+
+Required JS is bundled in this template to prevent it from cluttering core js files.
+This template is only rendered if Schul-Cloud OAuth provider is enabled.
+--}}
 @auth
     @php
         $identity = Auth::user()
@@ -8,8 +14,21 @@
         $iframe = $identity ? $identity->iframe : null;
     @endphp
     @if(!is_null($iframe))
-        <div class="userframe ui right aligned container">
+        <div id="userframe" class="userframe ui right aligned container" style="display: none">
             {!! $iframe !!}
         </div>
+        <script defer>
+            {{-- display Schul-Cloud iFrame if page is not embedded in iframe --}}
+            (function () {
+                var isDisplayedInFrame = window.top !== window.self;
+                if (isDisplayedInFrame) {
+                    return;
+                }
+                var frameContainer = document.getElementById('userframe');
+                if (frameContainer) {
+                    frameContainer.style.display = 'block';
+                }
+            })();
+        </script>
     @endif
 @endauth
